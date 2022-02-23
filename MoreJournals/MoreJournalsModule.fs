8d950b9c4b1fcda6edde8b109441b82cba665bf7
Aly <me@s5.pm>
2022-02-23 01:38:31 -0800

    Initial commit

---
+++ MoreJournals/MoreJournalsModule.fs
@@ -0,0 +1,18 @@
+﻿namespace Celeste.Mod.MoreJournals
+
+open Celeste.Mod
+
+type MoreJournalsModule() =
+    inherit EverestModule()
+    
+    override this.Load() =
+        On.Celeste.OuiMainMenu.add_OnBegin this.MainMenuOnBeginHook
+    
+    override this.Unload() =
+        On.Celeste.OuiMainMenu.remove_OnBegin this.MainMenuOnBeginHook
+    
+    member this.MainMenuOnBegin (orig: On.Celeste.OuiMainMenu.orig_OnBegin) (self: Celeste.OuiMainMenu): unit =
+        ()
+    
+    member this.MainMenuOnBeginHook =
+        On.Celeste.OuiMainMenu.hook_OnBegin this.MainMenuOnBegin
