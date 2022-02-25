1d7e581bb0750c3a8dda70798f8faa0c18b1873b
Aly <me@s5.pm>
2022-02-24 17:48:29 -0800

    Properly add a custom UI element

---
+++ MoreJournals/GlobalJournal.fs
@@ -0,0 +1,28 @@
+﻿namespace Celeste.Mod.MoreJournals
+
+open System.Collections
+
+type GlobalJournal() =
+    inherit Celeste.Oui()
+
+    override this.Enter(from: Celeste.Oui): IEnumerator =
+        upcast (seq {
+            Celeste.Audio.Play "event:/ui/world_map/journal/select" |> ignore
+            
+            yield null
+        }).GetEnumerator()
+    override this.Leave(next) =
+        upcast (seq {
+            Celeste.Audio.Play "event:/ui/world_map/journal/back" |> ignore
+            
+            yield null
+        }).GetEnumerator()
+
+    override this.Update() =
+        base.Update()
+        if this.Focused then
+            if Celeste.Input.MenuCancel.Pressed then this.Close()
+    
+    member this.Close() =
+        this.Overworld.Goto<Celeste.OuiMainMenu>()
+        |> ignore
--- MoreJournals/MoreJournalsModule.fs
+++ MoreJournals/MoreJournalsModule.fs
@@ -7,12 +7,23 @@ type MoreJournalsModule() =
     
     override this.Load() =
         On.Celeste.OuiMainMenu.add_OnBegin this.MainMenuOnBeginHook
+        On.Celeste.OuiMainMenu.add_Update this.MainMenuUpdateHook
     
     override this.Unload() =
         On.Celeste.OuiMainMenu.remove_OnBegin this.MainMenuOnBeginHook
+        On.Celeste.OuiMainMenu.remove_Update this.MainMenuUpdateHook
     
     member this.MainMenuOnBegin (orig: On.Celeste.OuiMainMenu.orig_OnBegin) (self: Celeste.OuiMainMenu): unit =
-        ()
+        orig.Invoke self
     
     member this.MainMenuOnBeginHook =
         On.Celeste.OuiMainMenu.hook_OnBegin this.MainMenuOnBegin
+    
+    member this.MainMenuUpdate (orig: On.Celeste.OuiMainMenu.orig_Update) (self: Celeste.OuiMainMenu): unit =
+        if self.Selected && self.Focused && Celeste.Input.MenuJournal.Pressed then
+            self.Overworld.Goto<GlobalJournal>()
+            |> ignore
+        orig.Invoke self
+    
+    member this.MainMenuUpdateHook =
+        On.Celeste.OuiMainMenu.hook_Update this.MainMenuUpdate
